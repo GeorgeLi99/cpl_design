e1ddd02e9675b5234836bd154b133d4e0263954d
Zhuoran_Li <m15677310992@163.com>
2025-05-16 22:48:07 +0800

    change stb image write file

--- src/main.c
+++ src/main.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "stb_image.h"
+#include "../third_party/stb/stb_image.h"
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2)
-    {
+    if (argc < 2) {
         fprintf(stderr, "Usage: %s <image_path>\n", argv[0]);
         return 1;
     }
     int width, height, channels;
-    unsigned char *data = stbi_load(argv[1], &width, &height, &channels, 0);
-    if (data == NULL)
-    {
-        fprintf(stderr, "Error loading '%s': %s\n", argv[1], stbi_failure_reason());
+    unsigned char *data = stbi_load("image.jpg", &width, &height, &channels, 0);
+    if (data == NULL) {
+        fprintf(stderr, "Error loading 'image.jpg': %s\n", stbi_failure_reason());
         exit(1);
     }
-    printf("Loaded '%s': %dx%d, %d channels\n", argv[1], width, height, channels);
+    printf("Loaded 'image.jpg': %dx%d, %d channels\n", width, height, channels);
     stbi_image_free(data);
     return 0;
 }
